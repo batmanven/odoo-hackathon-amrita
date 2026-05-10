d923d9530d2705a33fc2fa0ebbaa0764d080523f
supermanhenry <145008344+priyansh-narang2308@users.noreply.github.com>
2026-05-10 10:12:43 +0530

    feat: integrate Prisma ORM with LibSQL and configure Next.js environment for image domains and server actions

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,27 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '5mb',
+    },
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'plus.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'cdn.pixabay.com',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
